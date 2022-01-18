9dfe18d49e52d93aeeec397eca8d196db5f1f217
Ashley Terstriep <aterstriep@vervocity.io>
2022-01-17 18:14:15 -0600

    Server configuration

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import * as notes from "./notes.js";
+import path from "path";
 
 const router = express.Router();
 
-router.get("/notes", notes.list)
-router.post("/notes", notes.create)
-router.get("/notes/:id", notes.read)
-router.post("/notes/:id", notes.update)
-router.delete("/notes/:id", notes.deleteNote)
+router.get("/notes", notes.list);
+router.post("/notes", notes.create);
+router.get("/notes/:id", notes.read);
+router.post("/notes/:id", notes.update);
+router.delete("/notes/:id", notes.deleteNote);
+
+router.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..client/build", "index.html"));
+})
 
 export default router;

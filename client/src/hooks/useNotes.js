9d83310e9672d27dec368334882ab946acf33f12
Ashley Terstriep <aterstriep@vervocity.io>
2022-01-22 10:42:28 -0600

    Refactored useNotes hook

--- client/src/hooks/useNotes.js
+++ client/src/hooks/useNotes.js
@@ -1,11 +1,56 @@
 import { useEffect, useReducer, useState } from 'react'
 
+function reducer(state, action) {
+    switch (action.type) {
+        case "create":
+            return {
+                action: action.type,
+                method: "POST",
+                path: "/notes",
+                body: JSON.stringify(action.data),
+            }
+        case "update":
+            return {
+                action: action.type,
+                method: "POST",
+                path: `/notes/${action.data.id}`,
+                body: JSON.stringify(action.data),
+            }
+        case "read":
+            return {
+                action: action.type,
+                method: "GET",
+                path: `/notes/${action.data.id}`,
+            }
+        case "delete":
+            return {
+                action: action.type,
+                method: "DELETE",
+                path: `/notes/${action.data.id}`,
+            }
+        case "init":
+            return {
+                action: action.type,
+                method: "GET",
+                path: "/notes",
+            }
+        default:
+            throw new Error("missing action type");
+    }
+}
+
 export default function useNotes() {
 
     const [notes, setNotes] = useState([]);
     const [note, setNote] = useState({});
-    
-    const handleSetNotes = (data) => {
+
+    const [state, dispatch] = useReducer(reducer, {
+        action: "init",
+        method: "GET",
+        path: "/notes",
+    });
+
+    function handleSetNotes(data) {
         setNotes(data.notes);
         if (!data.notes.length) {
             dispatch({ data: { title: "", body: "" }, type: "create" });
@@ -14,100 +59,45 @@ export default function useNotes() {
         }
     }
 
-    const handleSetNote = (data) => {
-        setNote(data.note);
-    }
-
-    const deleteNote = (data) => {
-
+    function deleteNote(data) {
         if(data.id !== note.id) {
-            dispatch({type: "init"});
             return;
         }
-
         const index = notes.findIndex(note => note.id === data.id);
         if(index >= notes.length - 1) {
             setNote(notes[0]);
         } else {
             setNote(notes[index + 1]);
         }
-
     }
 
-    function reducer(state, action) {
-        switch (action.type) {
-            case "create":
-                return {
-                    action: action.type,
-                    method: "POST",
-                    path: "/notes",
-                    body: JSON.stringify(action.data),
-                    callback: handleSetNote
-                }
-            case "update":
-                return {
-                    action: action.type,
-                    method: "POST",
-                    path: `/notes/${action.data.id}`,
-                    body: JSON.stringify(action.data),
-                    callback: handleSetNote
-                }
-            case "read":
-                return {
-                    action: action.type,
-                    method: "GET",
-                    path: `/notes/${action.data.id}`,
-                    callback: handleSetNote
-                }
-            case "delete":
-                return {
-                    action: action.type,
-                    method: "DELETE",
-                    path: `/notes/${action.data.id}`,
-                    callback: deleteNote
-                }
-            case "init":
-                return {
-                    action: action.type,
-                    method: "GET",
-                    path: "/notes",
-                    callback: handleSetNotes
-                }
-            default:
-                throw new Error("missing action type");
+    async function execute() {
+        let options = {
+            method: state.method,
+            headers: {
+                "content-type": "application/json",
+            },
         }
+        options.body = state.body || null;
+        const response = await fetch(state.path || "/notes", options)
+        return await response.json();
     }
-
-    const [state, dispatch] = useReducer(reducer, {
-        action: "init",
-        method: "GET",
-        path: "/notes",
-        callback: handleSetNotes
-    });
     
     useEffect(() => {
-        function execute() {
-            let options = {
-                method: state.method,
-                headers: {
-                    "content-type": "application/json",
-                },
-            }
-            options.body = state.body || null;
-            const response = fetch(state.path || "/notes", options)
-                .then(response => response.json())
-                .then(data => {
-                    state.callback(data)
-                })
-                .catch(error => console.error(error));
-            return response;
-        }
-        execute();
+        execute() 
+            .then(response => {
+                if(["read", "update", "create"].includes(state.action)) {
+                    setNote(response.note);
+                    dispatch({type: "init"});
+                } else if (state.action === "delete") {
+                    deleteNote(response);
+                    dispatch({type: "init"});
+                } else {
+                    handleSetNotes(response);
+                }
+            })
+            .catch(error => console.log(error));
     }, [state])
-
-    useEffect(() => {
-      dispatch({type: "init"});
-    }, [note]);
     
     return [notes, note, dispatch]
 }
